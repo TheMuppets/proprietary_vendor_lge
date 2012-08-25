fa742096526be7e93b331c7529984e37f4620cff
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-08-25 18:46:11 +0100

    Add e610 blobs

--- p700/p700-vendor-blobs.mk
+++ p700/p700-vendor-blobs.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/p700/proprietary/lib/hw/lights.msm7x27a.so:system/lib/hw/lights.msm7x27a.so \
     vendor/lge/p700/proprietary/lib/hw/sensors.u0.so:system/lib/hw/sensors.u0.so \
     vendor/lge/p700/proprietary/bin/sensord:system/bin/sensord \
+    vendor/lge/p700/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/lge/p700/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/lge/p700/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/lge/p700/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
