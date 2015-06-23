9c950fca3b00dd07739972c79b70c59ef2458afc
Rashed Abdel-Tawab <rashed@linux.com>
2015-06-23 19:51:22 -0400

    Add ls980 ADSP images and d803 vendor folder

--- ls980/ls980-vendor-blobs.mk
+++ ls980/ls980-vendor-blobs.mk
@@ -18,3 +18,19 @@ PRODUCT_COPY_FILES += \
     vendor/lge/ls980/proprietary/lib/hw/nfc.default.so:system/lib/hw/nfc.default.so \
     vendor/lge/ls980/proprietary/lib/hw/nfc.lg.so:system/lib/hw/nfc.lg.so \
     vendor/lge/ls980/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so
+
+PRODUCT_COPY_FILES += \
+    vendor/lge/ls980/firmware/adsp.b00:system/etc/firmware/adsp.b00 \
+    vendor/lge/ls980/firmware/adsp.b01:system/etc/firmware/adsp.b01 \
+    vendor/lge/ls980/firmware/adsp.b02:system/etc/firmware/adsp.b02 \
+    vendor/lge/ls980/firmware/adsp.b03:system/etc/firmware/adsp.b03 \
+    vendor/lge/ls980/firmware/adsp.b04:system/etc/firmware/adsp.b04 \
+    vendor/lge/ls980/firmware/adsp.b05:system/etc/firmware/adsp.b05 \
+    vendor/lge/ls980/firmware/adsp.b06:system/etc/firmware/adsp.b06 \
+    vendor/lge/ls980/firmware/adsp.b07:system/etc/firmware/adsp.b07 \
+    vendor/lge/ls980/firmware/adsp.b08:system/etc/firmware/adsp.b08 \
+    vendor/lge/ls980/firmware/adsp.b09:system/etc/firmware/adsp.b09 \
+    vendor/lge/ls980/firmware/adsp.b10:system/etc/firmware/adsp.b10 \
+    vendor/lge/ls980/firmware/adsp.b11:system/etc/firmware/adsp.b11 \
+    vendor/lge/ls980/firmware/adsp.b12:system/etc/firmware/adsp.b12 \
+    vendor/lge/ls980/firmware/adsp.mdt:system/etc/firmware/adsp.mdt
