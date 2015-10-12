07fb2b6dc8686b189cd5ba0929d4d3e8c5ac55b4
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-11 21:03:46 -0500

    g3: update adreno from hammerhead-mra58k
    Change-Id: I45d613deee2dc512c8d674342ee7d0507b46f1c6

--- g3-common/g3-common-vendor-blobs.mk
+++ g3-common/g3-common-vendor-blobs.mk
@@ -177,6 +177,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g3-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
     vendor/lge/g3-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/lge/g3-common/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/lge/g3-common/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
