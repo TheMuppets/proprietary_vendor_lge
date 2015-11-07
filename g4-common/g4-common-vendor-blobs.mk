cd55cf920615aaa17821db8557a7d6dddc97a5e9
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-11-07 21:33:06 +0000

    g4: add liblqe for ril
    Change-Id: I34ef667dd00b53afdfcb5c649027c021d3cea72e

--- g4-common/g4-common-vendor-blobs.mk
+++ g4-common/g4-common-vendor-blobs.mk
@@ -786,5 +786,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/g4-common/proprietary/vendor/lib64/libflp.so:system/vendor/lib64/libflp.so \
     vendor/lge/g4-common/proprietary/etc/flp.conf:system/etc/flp.conf \
     vendor/lge/g4-common/proprietary/vendor/lib/hw/gatekeeper.msm8992.so:system/vendor/lib/hw/gatekeeper.msm8992.so \
-    vendor/lge/g4-common/proprietary/vendor/lib64/hw/gatekeeper.msm8992.so:system/vendor/lib64/hw/gatekeeper.msm8992.so
+    vendor/lge/g4-common/proprietary/vendor/lib64/hw/gatekeeper.msm8992.so:system/vendor/lib64/hw/gatekeeper.msm8992.so \
+    vendor/lge/g4-common/proprietary/vendor/lib/liblqe.so:system/vendor/lib/liblqe.so \
+    vendor/lge/g4-common/proprietary/vendor/lib64/liblqe.so:system/vendor/lib64/liblqe.so
 
