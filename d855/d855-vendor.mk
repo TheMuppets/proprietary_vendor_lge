2ce66485616bac85737c3229967e364b1c2e5f6e
Hendrik Hagendorn <git@finnq.de>
2016-12-28 14:50:37 +0100

    d851/d852/d855/vs985: Regenerate makefiles
    Change-Id: If792af891830a042a114bb4ad3ba8e0c59ff152d

--- d855/d855-vendor.mk
+++ d855/d855-vendor.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/d855/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
     vendor/lge/d855/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
     vendor/lge/d855/proprietary/etc/sensors/sensor_def_variable.conf:system/etc/sensors/sensor_def_variable.conf \
-    vendor/lge/d855/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/lge/d855/proprietary/lib/libuicc.so:system/lib/libuicc.so \
     vendor/lge/d855/proprietary/lib/libvss_common_idl.so:system/lib/libvss_common_idl.so \
     vendor/lge/d855/proprietary/lib/libvss_common_iface.so:system/lib/libvss_common_iface.so \
@@ -55,7 +54,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/d855/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
     vendor/lge/d855/proprietary/vendor/lib/liblqe.so:system/vendor/lib/liblqe.so \
     vendor/lge/d855/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
-    vendor/lge/d855/proprietary/vendor/lib/libperipheral_client.so:system/vendor/lib/libperipheral_client.so \
     vendor/lge/d855/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
     vendor/lge/d855/proprietary/vendor/lib/libqdi.so:system/vendor/lib/libqdi.so \
     vendor/lge/d855/proprietary/vendor/lib/libqdp.so:system/vendor/lib/libqdp.so \
@@ -69,3 +67,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/d855/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/lge/d855/proprietary/vendor/lib/libril-qc-qmi-1.so:system/vendor/lib/libril-qc-qmi-1.so \
     vendor/lge/d855/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so
+
+PRODUCT_PACKAGES += \
+    libmdmdetect \
+    libperipheral_client
