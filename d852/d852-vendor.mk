52ca750099cdda2646dc8e35a94bd8f5c55fb3ed
Brian J. Murrell <brian@interlinx.bc.ca>
2017-01-10 10:18:52 -0500

    d852: add libqmi_client_helper.so to the d852-vendor.mk
    The libqmi_client_helper.so proprietary file is missing from d852-vendor.mk.

--- d852/d852-vendor.mk
+++ d852/d852-vendor.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
     vendor/lge/d852/proprietary/vendor/lib/libqdp.so:system/vendor/lib/libqdp.so \
     vendor/lge/d852/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
     vendor/lge/d852/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
+    vendor/lge/d852/proprietary/vendor/lib/libqmi_client_helper.so:system/vendor/lib/libqmi_client_helper.so \
     vendor/lge/d852/proprietary/vendor/lib/libqmi_client_qmux.so:system/vendor/lib/libqmi_client_qmux.so \
     vendor/lge/d852/proprietary/vendor/lib/libqmi_common_so.so:system/vendor/lib/libqmi_common_so.so \
     vendor/lge/d852/proprietary/vendor/lib/libqmi_csi.so:system/vendor/lib/libqmi_csi.so \
