d2f2f83b82c850ce53c0c16a85a173ae50adf2f6
bryan2894 <bryan2894@gmail.com>
2017-03-14 02:54:59 -0400

    d850: remove prebuilt ril
    Change-Id: Iefe30d742dae34d47de51bc37018f661f409b85a

--- d850/Android.mk
+++ d850/Android.mk
@@ -18,4 +18,25 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),d850)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmdmdetect
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/lib/libmdmdetect.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libperipheral_client
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/vendor/lib/libperipheral_client.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
