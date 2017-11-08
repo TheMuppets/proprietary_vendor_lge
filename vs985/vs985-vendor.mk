a7ec0ab8fc9c2c026c54448b140c4e30c0d3b15b
firebird11 <hbgassel@gmail.com>
2017-11-08 23:30:56 +0100

    vs985: source built ril
    depends on: https://review.lineageos.org/#/c/196507/
    
    Change-Id: I25fcb4a4b88984f84fff8f34f925ae78722908dc

--- vs985/vs985-vendor.mk
+++ vs985/vs985-vendor.mk
@@ -26,6 +26,8 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/etc/acdbdata/Hdmi_cal.acdb:system/etc/acdbdata/Hdmi_cal.acdb \
     vendor/lge/vs985/proprietary/etc/acdbdata/Headset_cal.acdb:system/etc/acdbdata/Headset_cal.acdb \
     vendor/lge/vs985/proprietary/etc/acdbdata/Speaker_cal.acdb:system/etc/acdbdata/Speaker_cal.acdb \
+    vendor/lge/vs985/proprietary/etc/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    vendor/lge/vs985/proprietary/etc/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     vendor/lge/vs985/proprietary/etc/firmware/venus.b00:system/etc/firmware/venus.b00 \
     vendor/lge/vs985/proprietary/etc/firmware/venus.b01:system/etc/firmware/venus.b01 \
     vendor/lge/vs985/proprietary/etc/firmware/venus.b02:system/etc/firmware/venus.b02 \
@@ -33,7 +35,6 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04 \
     vendor/lge/vs985/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
     vendor/lge/vs985/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
-    vendor/lge/vs985/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/lge/vs985/proprietary/lib/libuicc.so:system/lib/libuicc.so \
     vendor/lge/vs985/proprietary/lib/libvss_common_idl.so:system/lib/libvss_common_idl.so \
     vendor/lge/vs985/proprietary/lib/libvss_common_iface.so:system/lib/libvss_common_iface.so \
@@ -49,16 +50,17 @@ PRODUCT_COPY_FILES += \
     vendor/lge/vs985/proprietary/vendor/lib/libdsi_netctrl.so:system/vendor/lib/libdsi_netctrl.so \
     vendor/lge/vs985/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
     vendor/lge/vs985/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
+    vendor/lge/vs985/proprietary/vendor/lib/liblqe.so:system/vendor/lib/liblqe.so \
     vendor/lge/vs985/proprietary/vendor/lib/libnetmgr.so:system/vendor/lib/libnetmgr.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqdi.so:system/vendor/lib/libqdi.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqdp.so:system/vendor/lib/libqdp.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
+    vendor/lge/vs985/proprietary/vendor/lib/libqmi_client_helper.so:system/vendor/lib/libqmi_client_helper.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi_client_qmux.so:system/vendor/lib/libqmi_client_qmux.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi_common_so.so:system/vendor/lib/libqmi_common_so.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi_csi.so:system/vendor/lib/libqmi_csi.so \
-    vendor/lge/vs985/proprietary/vendor/lib/libqmi_csvt_srvc.so:system/vendor/lib/libqmi_csvt_srvc.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmi_encdec.so:system/vendor/lib/libqmi_encdec.so \
     vendor/lge/vs985/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/lge/vs985/proprietary/vendor/lib/libril-qc-qmi-1.so:system/vendor/lib/libril-qc-qmi-1.so \
