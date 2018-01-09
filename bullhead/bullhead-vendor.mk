24c9905dabb500bee07d175ce233ee27fc84685d
razorloves <razorloves@gmail.com>
2018-01-09 00:43:17 -0600

    bullhead: blobs update from OPM3.171019.013 factory image
    Change-Id: If347f8b9317a347cd8a9154b77cc2de9a9f65eb5

--- bullhead/bullhead-vendor.mk
+++ bullhead/bullhead-vendor.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2015-2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -90,7 +90,9 @@ PRODUCT_PACKAGES += \
     DMService \
     DiagMon \
     GCS \
-    HotwordEnrollmentWCD9330 \
+    HotwordEnrollmentOKGoogleWCD9330 \
+    HotwordEnrollmentTGoogleWCD9330 \
+    HotwordEnrollmentXGoogleWCD9330 \
     LifeTimerService \
     SprintDM \
     atfwd \
