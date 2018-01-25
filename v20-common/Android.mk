9b8d1a630c1b3d52ba7187883fa04a8054fd08bb
Eric Meddaugh <etmsys@cpu.rit.edu>
2018-01-24 20:19:30 -0500

    h990: regen makefiles to add to build guard
    This add h990 to the common make files:
    * msm8896-common/Android.mk
    * v20-common/Android.mk

--- v20-common/Android.mk
+++ v20-common/Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter v20 h910 h918 us996 ls997 vs995,$(TARGET_DEVICE)),)
+ifneq ($(filter v20 h910 h915 h918 h990 vs995 us996 ls997,$(TARGET_DEVICE)),)
 
 endif
