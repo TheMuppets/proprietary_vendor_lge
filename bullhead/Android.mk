f16addaabed8e944627237eb360ca40095596f38
razorloves <razorloves@gmail.com>
2018-03-03 22:38:52 -0600

    bullhead: add google camera dependencies
    Starting with google camera version 4.4.020 these 2 files are needed.
    Extracted from OPM3.171019.014 bullhead factory image.
    
    Change-Id: Ibe170f52f5eb14d03b60c8af6d798e9f315b19fc

--- bullhead/Android.mk
+++ bullhead/Android.mk
@@ -265,6 +265,16 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.android.camera.experimental2016
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/framework/com.google.android.camera.experimental2016.jar
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilhook
 LOCAL_MODULE_OWNER := lge
 LOCAL_SRC_FILES := proprietary/framework/qcrilhook.jar
