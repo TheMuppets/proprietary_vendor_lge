801361358e5507af742527ecf82330b5e3cd9cbe
Eric Meddaugh <eric@meddaughs.com>
2019-07-08 08:16:57 -0400

    msm8996-common: Add QCOM WFD stack
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    * From: OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1906252002:user/release-keys

--- msm8996-common/Android.mk
+++ msm8996-common/Android.mk
@@ -352,6 +352,28 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := WfdService
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/priv-app/WfdService/WfdService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := WfdCommon
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := proprietary/framework/WfdCommon.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := QtiTelephonyServicelibrary
 LOCAL_MODULE_OWNER := lge
 LOCAL_SRC_FILES := proprietary/framework/QtiTelephonyServicelibrary.jar
